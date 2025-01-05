599d902166b6acf95bb0433cd9010e7ee219ec11
amitb-21 <amitkumarbehera2104@gmail.com>
2025-01-05 15:58:07 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,20 @@ import Auth from "./components/Auth"
 export default function App() {
   const apiKey = 'p8szgw9spkzu';
   const client = StreamChat.getInstance(apiKey);
+  const cookies = new Cookies();
 
-  const authToken = false;
+  const authToken = cookies.get('token');
+
+  if(authToken){
+    client.connectUser({
+      id:cookies.get('userId'),
+      name:cookies.get('username'),
+      fullName:cookies.get('fullName' ),
+      phoneNumber:cookies.get('phoneNumber'),
+      image:cookies.get('avatarURL'),
+      hashedPassword:cookies.get('hashedPassword'),
+    }, authToken)
+  }
   
   if(!authToken) return <Auth/>
   return (
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import signInImage from '../assets/signup.jpg'
 import { Avatar } from 'stream-chat-react';
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: '',
     username: '',
@@ -22,9 +24,28 @@ export default function Auth() {
 
     }
 
-    const handleSubmit  = (e) => {
+    const handleSubmit  = async (e) => {
         e.preventDefault();
-        console.log(form);
+        const { fullName, username, password, phoneNumber, avatarURL }= form;
+
+        const URL = 'http://localhost:5000/auth';
+
+        const { data: {token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`,{
+            username, password, fullName, phoneNumber, avatarURL
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     const switchMode = () => {
